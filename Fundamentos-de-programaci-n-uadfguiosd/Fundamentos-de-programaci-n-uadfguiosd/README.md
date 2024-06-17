bcc92637767c6409b6d2e4eb87dc99accce227dc
KenzoDev1 <167940886+KenzoDev1@users.noreply.github.com>
2024-06-17 19:00:03 -0400

    ahora si que si, este es la version final XD

---
+++ Fundamentos-de-programaci-n-uadfguiosd/Fundamentos-de-programaci-n-uadfguiosd/README.md
@@ -0,0 +1,12 @@
+# Fundamentos-de-programaci-n
+Repositorio de fundamentos de programacion ing en informatica 1er semestre
+integrantes 
+Ignacio Bittner 
+Diego Mansilla
+Thomas Martinez
+
+Login: 
+
+usuario: nachobittner@duocuc.cl
+
+contraseña: 1234
---
+++ Fundamentos-de-programaci-n-uadfguiosd/Fundamentos-de-programaci-n-uadfguiosd/inicio sesion.py
@@ -0,0 +1,34 @@
+import time
+import os
+from menu_de_opciones import menu
+while True:
+    try: 
+        #Limpiar pantalla
+        os.system("cls")
+        print("<---Empresas Calfun--->")
+        print("1.-Iniciar Sesion")
+        print("2.-Salir")
+        opcion_usuario=int(input("\nSeleccione su opcion --> "))
+        os.system("cls")
+    except ValueError:
+        print("Ingreso un valor invalido, por favor vuelva a intentarlo...")
+        time.sleep(2)
+    #Inicio sesion
+    if opcion_usuario==1:
+        print ("Ingrese su correo")
+        usuario = input("--> ")
+        print ("Ingrese su contraseña")
+        contraseña = input("--> ")
+        if usuario == ("nachobittner@duocuc.cl") and (contraseña == "1234"):
+            print("Inicio de sesion exitoso!")
+            break
+    #Salir
+    elif opcion_usuario==2:
+        print("Has elegido salir");
+        #Funcion exit(), fuerza al programa a finalizar la ejecucion
+        exit()
+    else:
+        print("Ingreso una opcion invalida, por favor vuelva a intentarlo...");
+        time.sleep(2)
+menu()
+#Actualizado
